a8ddcd5922c1318496fe67aaeacaa939eaa6e656
Madhuri <Madhuri@10.1.100.79>
2018-06-12 10:04:17 -0500

    Tested else-if statement

--- YashTraining/src/com/yash/HelloWorld.java
+++ YashTraining/src/com/yash/HelloWorld.java
@@ -3,8 +3,21 @@ package com.yash;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-   System.out.println("Hello Everyone, I will add new code");
+		
+		int i= 15;
+		
+		if (i==5) {
+			
+			System.out.println(" i is 5");
+			
+			
+		}else if (i==10) {
+			System.out.println("i is 10");
+		}else {
+			System.out.println("we dont have i value");
+		}
+		
+		
 	}
 
 }
