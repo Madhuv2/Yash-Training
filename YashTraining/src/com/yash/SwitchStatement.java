66e9a21cecf0a2e04ca6336f72ffcf3eb49e03a1
Madhuri <Madhuri@10.1.100.79>
2018-06-12 10:40:30 -0500

    Tested switch statement

---
+++ YashTraining/src/com/yash/SwitchStatement.java
@@ -0,0 +1,25 @@
+package com.yash;
+
+public class SwitchStatement {
+	
+	public static void main(String[] args) {
+		
+		String s = "Hello";
+		
+		switch(s) {
+		
+		case "Hi": System.out.println("Hi");
+		   break;
+default:
+			
+			System.out.println("Bye");
+		case "Hello": System.out.println("Hello");
+		   break;
+		
+		
+		
+		}
+		
+	}
+
+}
