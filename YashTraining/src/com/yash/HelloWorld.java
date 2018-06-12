907e06db1eeda70da56e744d96e994d9e8e17630
Madhuri <Madhuri@LAPTOP-QI4JT35Q>
2018-06-12 07:12:48 -0500

    Second commit

--- YashTraining/src/com/yash/HelloWorld.java
+++ YashTraining/src/com/yash/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("Hello Everyone!");
+   System.out.println("Hello Everyone, I will add new code");
 	}
 
 }
